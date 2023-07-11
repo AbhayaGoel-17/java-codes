1d2b2930aac711f25feac2663029f2a7c056d392
AbhayaGoel-17 <139268000+AbhayaGoel-17@users.noreply.github.com>
2023-07-12 00:25:43 +0530

    Add files via upload

---
+++ odd_even.java
@@ -0,0 +1,17 @@
+package Lec1;
+
+public class odd_even {
+
+	public static void main(String[] args) {
+		int n=899864316;
+		if (n % 2 == 0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
